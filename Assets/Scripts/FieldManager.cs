54aea49727031ba523bf5e8dce88e9833083dfeb
Xavier Greer <work.xavier.greer@gmail.com>
2020-10-03 17:27:22 -0600

    First Commit, Level Built

---
+++ Assets/Scripts/FieldManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldManager : MonoBehaviour
+{
+    //Vector3 TestPos;
+    Transform TestTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TestPos = transform.position;
+        //print(TestPos);
+        //TestPos = new Vector3(0f, 10f, 0f);
+        //print(TestPos);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
